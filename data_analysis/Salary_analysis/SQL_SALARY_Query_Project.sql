688542babf2d6df6c827d476b80ca65cd4d5dff5
Sinamandla <121508711+sinamandla1@users.noreply.github.com>
2023-07-26 15:29:40 +0200

    Update SQL_SALARY_Query_Project.sql
    Numbered each query.

--- data_analysis/Salary_analysis/SQL_SALARY_Query_Project.sql
+++ data_analysis/Salary_analysis/SQL_SALARY_Query_Project.sql
@@ -2,6 +2,7 @@
 
 -----SKILLS USED: CTE, MAX Function, CASES, Temp tables and Converting data types
 
+--1.
 ---After importing the csv file to Excel and converting it into a table and cleaning
 ---Check if the query results correspond with the excel file table shape.
 
@@ -15,6 +16,7 @@ SELECT Gender,
   Salary
 FROM Salary_Data_Based_country_and_r$
 
+--2.
 ---Query the structure to find out what roles youths (30 years and younger) are employed in,
 ---using DISTINCT to give cleaner results.
 
@@ -30,6 +32,7 @@ FROM Salary_Data_Based_country_and_r$
 WHERE TRY_CAST(Age AS FLOAT) <= 30.0
 ORDER BY Age, [Years of Experience];
 
+--3.
 ---Querying the structure to see the gender is distributed in this data set. With this
 ---we can assess the odds of getting employed as a specific gender aged 30yrs and younger,
 ---or to assess current industry policies.
@@ -40,6 +43,7 @@ SELECT
 FROM Salary_Data_Based_country_and_r$
 WHERE TRY_CAST(Age AS FLOAT) <= 30.0;
 
+--4.
 ---Querying the structure to see the gender is distributed in this data set. With this
 ---we can assess the odds of getting employed as a specific gender, or to assess current
 ---industry or organisational policies.
@@ -49,6 +53,7 @@ SELECT
  SUM(CASE WHEN Gender = 'female' THEN 1 ELSE 0 END) AS 'Total Females Working'
 FROM Salary_Data_Based_country_and_r$
 
+--5.
 ---Querying the structure to get the various Job Titles in the data set and the
 ---number of people employed in each role.
 
@@ -58,6 +63,7 @@ FROM Salary_Data_Based_country_and_r$
 GROUP BY [Job Title]
 ORDER BY [Number of employees] DESC;
 
+--6.
 ---Querying the structure to get the various Job Titles in the data set and the
 ---number of people aged 30yrs and younger employed in each role.
 
@@ -68,6 +74,7 @@ WHERE TRY_CAST(Age AS FLOAT) <= 30.0
 GROUP BY [Job Title]
 ORDER BY [Number of employees 30 and younger] DESC;
 
+--7.
 ---Ranking each country according to how many people it employs. This would
 ---help people looking for employment to set the sights onto places with high
 ---numbers of employment to take advantage of the higher odds available.
@@ -78,6 +85,7 @@ FROM Salary_Data_Based_country_and_r$
 GROUP BY Country
 ORDER BY [Number of employees] DESC;
 
+--8.
 ---Ranking each country according to how many Software engineers it employs. 
 ---This would help people looking for employment to set the sights onto places
 ---with high numbers of employment to take advantage of the higher odds available.
@@ -89,6 +97,7 @@ WHERE [Job Title] = 'Software Engineer'
 GROUP BY Country
 ORDER BY [Number of Software Engineers] DESC;
 
+--9.
 ---Assessing the distribution of genders in the number of Software Engineers
 ---employed. This will show how inclusive the job role is. We also see how socio-economic
 ---and industry policies are performing.
@@ -100,6 +109,7 @@ FROM Salary_Data_Based_country_and_r$
 WHERE [Job Title] = 'Software Engineer'
 GROUP BY [Job Title];
 
+--10.
 ---Assessing the distribution of genders in the number of Software Engineers
 ---employed, aged 30yrs and younger. This will show how inclusive the job role is.
 ---We also see how socio-economic and industry policies are performing.
@@ -111,6 +121,7 @@ FROM Salary_Data_Based_country_and_r$
 WHERE [Job Title] = 'Software Engineer' AND TRY_CAST(Age AS FLOAT) <= 30.0
 GROUP BY [Job Title];
 
+--11.
 ---Ranking the employment in each country according to the distribution of genders involved.
 
 SELECT Country,
@@ -120,6 +131,7 @@ FROM Salary_Data_Based_country_and_r$
 GROUP BY Country
 ORDER BY 2,3 DESC;
 
+--12.
 ---looking at the distribution of genders in the highest paying field in the data set,
 ---within a specific country. We can see how effective their socio-economic environment is.
 
@@ -131,6 +143,7 @@ WHERE [Job Title] = 'Software Engineer' AND TRY_CAST(Age AS FLOAT) <= 30.0
 GROUP BY Country
 ORDER BY 2 DESC;
 
+--13.
 ---specifically looking at the highest paying job, we want to see how the picture
 ---looks racially and look to see whether empowerment programs are needed.
 
@@ -140,6 +153,7 @@ FROM Salary_Data_Based_country_and_r$
 GROUP BY Race
 ORDER BY [Number of employees] DESC;
 
+--14.
 ---specifically looking at the highest paying job, we also want to see how the picture
 ---looks racially for the youth population.
 
@@ -150,6 +164,7 @@ WHERE TRY_CAST(Age AS FLOAT) <= 30.0
 GROUP BY Race
 ORDER BY [Number of employees 30 and under] DESC;
 
+--15.
 ---Ranking the Job roles according to the highest salary and most years of experience
 ---for career choice sake, we would use this to know which part of the market to work
 ---towards for a meaningful career.
